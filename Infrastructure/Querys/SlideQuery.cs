64bed89f17bf48d5e1198faff3b4c4857107886f
PiaggioFlor <piaggioflorr@gmail.com>
2025-04-27 17:12:12 -0300

    Crear y modificar Slide

---
+++ Infrastructure/Querys/SlideQuery.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces.Querys;
+using Domain.Entities;
+using Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Querys
+{
+    public class SlideQuery:ISlideQuery
+    {
+        private readonly ServiceContext _context;
+
+        public SlideQuery (ServiceContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Slide> GetSlideId(int slideId)
+        {
+            var slide = await _context.Slides.FindAsync(slideId);
+
+            return slide;
+        }
+    }
+}
